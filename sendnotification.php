733e684b6567181579a7196f5002f7669049f475
Alex Wilson <alexwilson2012@u.northwestern.edu>
2012-11-07 01:14:51 -0600

    Button on sendmylocation.html sends and email to alexwilson2012@u.northwestern.edu with lat/long

--- sendnotification.php
+++ sendnotification.php
@@ -1,14 +1,14 @@
 <?php
 
 $to      = 'AlexWilson2012@u.northwestern.edu';
-$subject = 'the subject';
-$message = 'hello';
+$subject = 'TakeControl - Notification';
+$message = 'Alex has arrived at Latitude ' . $_POST['Latitude'] . ', and Longitude ' . $_POST['Longitude'] . '. Unfamiliar with this location? Here\'s a map.';
 $headers = 'From: AlexWilson2012@u.northwestern.edu' . "\r\n" .
     'Reply-To: AlexWilson2012@u.northwestern.edu' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
 
-echo 'hi';
+echo 'We have notified ' . $to . ' that you have arrived!';
 
 ?>
