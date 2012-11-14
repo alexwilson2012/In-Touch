b9e29cb0474f832e9204d0fb1677e6fb658918e9
Alex Wilson <alexwilson2012@u.northwestern.edu>
2012-11-14 00:33:24 -0600

    fixed email robustness for spaces and ' " characters. also added links to shuffle between pages

--- send_comment_email.php
+++ send_comment_email.php
@@ -1,7 +1,10 @@
 <?php
 
+$_POST['comments'] = str_replace('"', '\"', $_POST['comments']);
+$_POST['comments'] = str_replace("'", "\'", $_POST['comments']);
+
 // Execute python script on command line
-echo exec('python comment_email.py '.$_POST['email'].' '.$_POST['name'].' "'.$_POST['comments'] . '"');
+echo exec('python comment_email.py "'.$_POST['email'].'" "'.$_POST['name'].'" "'.$_POST['comments'] . '"');
 
 // Send confirmation message back to the user
 echo 'Thanks for sending us your feedback!';
