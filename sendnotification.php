a59f2c104a097e2ad445bcb63da86281c0f07377
Irsal Jasebel <ijas@u.northwestern.edu>
2012-11-16 16:29:05 -0600

    minor change in php

--- sendnotification.php
+++ sendnotification.php
@@ -6,11 +6,11 @@ echo exec('python send_email.py '.$_POST['email'].' '.$_POST['Latitude'].' '.$_P
 // Send confirmation message back to the user
 if($_POST['preset_location'] == '')
 {
-	echo 'Hi, ' . $_POST['name'] . '. We have notified ' . $_POST['parent_name'] . ' that you have arrived!';
+	echo 'Hi, ' . $_POST['name'] . '. We have notified ' . $_POST['phone_email'] . ' that you have arrived!';
 }
 else
 {
-	echo 'Hi, ' . $_POST['name'] . '. We have notified ' . $_POST['parent_name'] . ' that you have arrived at '.$_POST['preset_location'].'!';
+	echo 'Hi, ' . $_POST['name'] . '. We have notified ' . $_POST['email'] . ' that you have arrived at '.$_POST['preset_location'].'!';
 }
 
 ?>
