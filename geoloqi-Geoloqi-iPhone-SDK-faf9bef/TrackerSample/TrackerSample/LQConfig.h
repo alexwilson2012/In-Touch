02c942064485e2fe49da10bc56548e8b6f259816
Irsal Jasebel <ijas@u.northwestern.edu>
2012-11-18 17:34:09 -0600

    geoloqi backbone

---
+++ geoloqi-Geoloqi-iPhone-SDK-faf9bef/TrackerSample/TrackerSample/LQConfig.h
@@ -0,0 +1,6 @@
+
+// Create an application on the Geoloqi developers site and
+// enter your API Key here.
+// https://developers.geoloqi.com/
+
+static NSString *const LQ_APIKey = @"dda19be85de3af2fb6ff607b5d9beec4";
