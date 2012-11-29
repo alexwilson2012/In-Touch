87e667335e47edb3ab303e990e82da59a1ae84cf
Alex Wilson <alexwilson2012@u.northwestern.edu>
2012-11-29 01:14:01 -0600

    removed geoloqi files

--- www/geoloqi-Geoloqi-iPhone-SDK-faf9bef/TrackerSample/TrackerSample/LQConfig.h
+++
@@ -1,6 +0,0 @@
-
-// Create an application on the Geoloqi developers site and
-// enter your API Key here.
-// https://developers.geoloqi.com/
-
-static NSString *const LQ_APIKey = @"dda19be85de3af2fb6ff607b5d9beec4";
